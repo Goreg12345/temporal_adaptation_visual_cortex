2bae55c8f2870657e49e7f0c1663d2870d86983a
Goreg12345 <georglange@gmx.de>
2023-06-08 02:35:04 +0200

    make divisive norm more performant

--- modules/divisive_norm.py
+++ modules/divisive_norm.py
@@ -5,31 +5,27 @@ import torch.nn.functional as F
 
 
 class DivisiveNorm(nn.Module):
-    def __init__(self, epsilon, K_init, train_K, alpha_init, train_alpha, sigma_init, train_sigma ):
+    def __init__(self, epsilon, K_init, train_K, alpha_init, train_alpha, sigma_init, train_sigma):
         super().__init__()
 
-        self.epsilon = epsilon  # 1.e-8
+        self.epsilon = epsilon
 
-        self.K = nn.Parameter(torch.tensor(K_init, dtype=float), requires_grad=train_K)
-        self.alpha = nn.Parameter(torch.tensor(alpha_init, dtype=float), requires_grad=train_alpha)
-        self.sigma = nn.Parameter(torch.tensor(sigma_init, dtype=float), requires_grad=train_sigma)
+        self.K = nn.Parameter(torch.tensor(K_init, dtype=torch.float32), requires_grad=train_K)
+        self.alpha = nn.Parameter(torch.tensor(alpha_init, dtype=torch.float32), requires_grad=train_alpha)
+        self.sigma = nn.Parameter(torch.tensor(sigma_init, dtype=torch.float32), requires_grad=train_sigma)
 
     def get_init_actvs(self, x, num_layer):
-        """ Get the initial activations. """
-        init_actvs = (torch.zeros_like(x, requires_grad=False), torch.zeros_like(x, requires_grad=False))
-        return init_actvs
+        return [torch.zeros_like(x, requires_grad=False)]
 
-    def forward(self, x, F_prev, G_prev):
-        L = torch.relu(x)  # linear response
+    def forward(self, x, G_prev):
+        L = torch.relu(x)
 
-        if G_prev.max() > self.K:                                                  # rescale if feedback signal exceeds maximal attainable response
-            G_prev = G_prev / G_prev.max()
-            G_prev = G_prev * self.K
+        G_max = G_prev.max()
+        if G_max > self.K:
+            G_prev = (G_prev / G_max) * self.K
 
-        F = torch.sqrt(self.K - G_prev + self.epsilon) / self.sigma  # multiplicative feedback
-        x = torch.relu(L * F)  # response
+        F = torch.sqrt(self.K - G_prev + self.epsilon) / self.sigma
+        response = torch.relu(L * F)
 
-        # compute G by adding activity of unit
-        G = ((1 - self.alpha) * G_prev) + self.alpha * x  # update feedback signal
-
-        return x, F, G
+        G = ((1 - self.alpha) * G_prev) + self.alpha * response
+        return response, G
